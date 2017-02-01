bf7a6dffeef74f07b0f8b28877ece7efa138aedb
fengfudev <fengfu.dev@gmail.com>
2017-02-01 16:53:51 +1100

    add firebase hosting

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import Vue from 'vue'
-import store from 'components/store/appStore'
-import fireStore from 'components/store/fireStore'
+import store from './components/store/appStore'
+import fireStore from './components/store/fireStore'
 import router from './router'
 require( './initialize' )
+// import 'jquery/dist/jquery.js'
+import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap/dist/js/bootstrap.js'
 
 import App from './App'
 
